783d33f6a75d8f9a1173893e3bef061db4f4963f
zagabur <76558764+zagabur@users.noreply.github.com>
2021-01-13 21:57:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// // <!-- 
+// // Задание: (Рандомные примеры)
+// //     Пользователь вводит колличество решаемых им примеров
+// //     И функций. Пользователю отдают рандомные числа и рандомные знаки для решаемых примеров
+// //     После примера отдается сообщение:
+// //         Ваш ответ верный - 2 
+// //             или
+// //         Ваш ответ не верный - 2! Правильный ответ - 3!
+// // -->
+
+
+
+function random(min, max) {
+    let randomNumber = Math.floor(Math.random() * (max + 1 - min) + min );
+    return randomNumber;
+}
+
+let minNumber = +prompt  ('Введите минимальное число', 1),
+   maxNumber = +prompt  ('Введите максимальное число', 15);
+
+
+for (let i = 0; i < 10; i++) {
+    let  num1 = random(minNumber, maxNumber);
+    let  num2 = random(minNumber, maxNumber);
+
+
+
+    let equasion =+ prompt (num1 +' + '+ num2 +' =? ');
+    let answer = (num1+num2) == equasion ? ' Молодец, это правильный ответ'  :  'К сожалению ваш ответ "'+ equasion + '" неправильный.';  
+    console.log(num1+' + '+ num2 + '=' +' ' +(num1 + num2) + ' Ваш ответ ' + equasion + '    ' + answer) ;
+    
+}
